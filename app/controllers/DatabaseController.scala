811654ecfbc7b1674fd9f31a38007767bf3ae08f
Harold Salaila <h.salaila@gmail.com>
2016-06-06 10:28:22 +0200

    Update DatabaseController with function to insert predefined articles

--- app/controllers/DatabaseController.scala
+++ app/controllers/DatabaseController.scala
@@ -2,9 +2,12 @@ package controllers
 
 import javax.inject.Inject
 
-import models.{ArticleRepo, PinboardArticleRepo, PinboardRepo}
+import models.{Article, ArticleRepo, PinboardArticleRepo, PinboardRepo}
 import play.api.mvc.{Action, Controller}
 import slick.driver.H2Driver.api._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
   * Created by Harold on 2016-06-05.
@@ -17,12 +20,19 @@ class DatabaseController @Inject() (articleRepo: ArticleRepo,
 
   def generateDDL = {
     val schemas = articleRepo.articles.schema ++ pinboardRepo.pinboards.schema ++ pinboardArticleRepo.pinboardArticles.schema
-    var ddl = ("# --- !Ups" + System.lineSeparator())
+    var ddl = "# --- !Ups" + System.lineSeparator()
     ddl += schemas.createStatements.foldLeft("")((ddl, statement) => ddl + statement + ";" + System.lineSeparator())
-    ddl += (System.lineSeparator() + "# --- !Downs" + System.lineSeparator())
+    ddl += System.lineSeparator() + "# --- !Downs" + System.lineSeparator()
     ddl += schemas.dropStatements.foldLeft("")((ddl, statement) => ddl + statement + ";" + System.lineSeparator())
     Action {
       Ok(ddl).as("text/plain")
+    }
+  }
+
+  def insertArticles = {
+    val listArticles = List(Article(0, "hello", "world"), Article(0, "Good bye", "world"))
+    Action {
+      Ok("Articles added: " + Await.result(articleRepo.addListArticle(listArticles), Duration.Inf)).as("text/plain")
     }
   }
 }
